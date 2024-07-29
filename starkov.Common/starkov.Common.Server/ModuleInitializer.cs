e7bb4fd2e70b4777af043e9aa97ff8a92ab1996b
Vershinin <vershinin@starkovgrp.ru>
2024-07-29 16:41:37 +0500

    Выдача прав на задачу + локализация

--- starkov.Common/starkov.Common.Server/ModuleInitializer.cs
+++ starkov.Common/starkov.Common.Server/ModuleInitializer.cs
@@ -9,5 +9,21 @@ namespace starkov.Common.Server
 {
   public partial class ModuleInitializer
   {
+
+    public override void Initializing(Sungero.Domain.ModuleInitializingEventArgs e)
+    {
+      GrantAccessRights();
+    }
+    
+    /// <summary>
+    /// Выдача прав на типы сущностей.
+    /// </summary>
+    public void GrantAccessRights()
+    {
+      var allUsers = Roles.AllUsers;
+      
+      TaskForApprovals.AccessRights.Grant(allUsers, DefaultAccessRightsTypes.Create);
+      TaskForApprovals.AccessRights.Save();
+    }
   }
 }
